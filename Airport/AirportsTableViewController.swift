bc79b8469691603590845f95d6f11cef567ca7cf
Mathieu Knook <mmaknook@pc131361.lan>
2017-10-20 16:43:48 +0200

    sections

--- Airport/AirportsTableViewController.swift
+++ Airport/AirportsTableViewController.swift
@@ -13,39 +13,60 @@ class AirportsTableViewController: UITableViewController {
     @IBOutlet var tabelAirport: UITableView!
     
     var airports = [Airport]()
-    
+    var airportSets = [(key: Character, value: [Airport])]()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        getAirports();
+    }
+    
+    func getAirports() {
         airports = DBManager.shared.getAllAirports()
+        
+        airports.forEach {
+            airport in
+            
+            var tempHeader: Character = "A"
+            
+            if airport.name?.isEmpty == false {
+                tempHeader = (airport.name?.characters.first)!
+            }
+            
+            if airportSets.contains(where:{$0.0 == tempHeader}) {
+                airportSets[airportSets.index(where: {$0.0 == tempHeader})!].value.append(airport)
+            } else {
+                airportSets.append((tempHeader, [airport]))
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return airportSets.map {String($0.key)}
+    }
+    
+    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return airportSets.index(where: {$0.key == title.characters.first!})!
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return airportSets.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(airportSets[section].key)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return airports.count
+        return airportSets[section].value.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cellIdentifier = "AirportTableViewCell"
-        
-        /*
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? AirportTableViewCell else {
-            fatalError("The dequeued cell is not an instance of AirportTableViewCell.")
-        }
-         */
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! AirportTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AirportTableViewCell", for: indexPath) as! AirportTableViewCell
         
         let airport = airports[indexPath.row]
 
@@ -54,51 +75,4 @@ class AirportsTableViewController: UITableViewController {
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
